15c1b9d5db6a0dbfee9ba32f9dbba7288f1baf2b
Harrison <harrison@theshoebridges.com>
2016-01-25 12:22:42 +1100

    Add basic platformer game

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # KatJump
+
+KatJump is a clone of the popular 2d platformer/fighter game [Sky Jump](http://www.gamesbutler.com/game/3681/Sky_Jump/), except it features online multiplayer support!
+
+## The Game
+
+You play as one of two characters, each trying to knock the other off of the platforms moving around the world.
--- js/main.js
+++ js/main.js
@@ -1,13 +1,51 @@
 var game = new Phaser.Game(800, 600, Phaser.AUTO, 'katjump', { preload: preload, create: create, update: update });
 
 function preload() {
-
+    game.load.image('player_one', 'img/player_one.png');
+    game.load.image('platform', 'img/platform.png');
 }
 
-function create() {
+var playerOne;
+var platforms;
+var cursors;
 
+var jumpTime = 0;
+
+function create() {
+    game.physics.startSystem(Phaser.Physics.ARCADE);
+
+    game.physics.arcade.gravity.y = 250;
+
+    playerOne = game.add.sprite(0, 0, 'player_one');
+    game.physics.enable(playerOne);
+
+    platforms = game.add.physicsGroup();
+
+    for (var i = 0; i < 5; i++) {
+        var p = platforms.create(game.rnd.between(0, game.width), game.rnd.between(0, game.height), 'platform');
+    }
+
+    platforms.setAll('body.allowGravity', false);
+    platforms.setAll('body.immovable', true);
+
+    cursors = game.input.keyboard.createCursorKeys();
 }
 
 function update() {
+    game.physics.arcade.collide(playerOne, platforms);
 
+    playerOne.body.velocity.x = 0;
+
+    if (cursors.right.isDown) {
+        playerOne.body.velocity.x = 200;
+    }
+
+    if (cursors.left.isDown) {
+        playerOne.body.velocity.x = -200;
+    }
+
+    if (cursors.up.isDown && (playerOne.body.touching.down || playerOne.body.onFloor()) && game.time.now > jumpTime) {
+        playerOne.body.velocity.y = -300;
+        jumpTime = game.time.now + 750;
+    }
 }
